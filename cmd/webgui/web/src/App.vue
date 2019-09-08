cd7c2b44879ce2eb3c03fdc0e68a8f8b65f4d4f6
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-09-08 15:47:24 -0300

    Add vue web gui

---
+++ cmd/webgui/web/src/App.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  #app
+    #nav
+      router-link.mr-1(to="/")
+        | Crear Árbol
+      router-link(to="/about")
+        | Info
+    router-view
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.mr-1 {
+  margin-right: 10px
+}
+</style>
