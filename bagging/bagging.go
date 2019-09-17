610ed95ea1087059d505816cb7b1a643bc3a554e
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-09-17 17:48:22 -0300

    Add classify method

--- bagging/bagging.go
+++ bagging/bagging.go
@@ -3,7 +3,6 @@ package bagging
 import (
 	"math/rand"
 
-	"github.com/Giulianos/ml-decision-tree/classifier"
 	"github.com/Giulianos/ml-decision-tree/decisiontree"
 )
 
@@ -18,11 +17,6 @@ func NewBagging(predAttr string, quantity int, seed int64) Bagging {
 		classifiers: make([]decisiontree.DecisionTree, quantity),
 		rng:         rand.New(rand.NewSource(seed)),
 	}
-}
-
-func (b Bagging) Classify(example classifier.Example) (string, float64) {
-	// TODO: implement method
-	return "", 0
 }
 
 func (b Bagging) GetClasses() []string {
---
+++ bagging/classify.go
@@ -0,0 +1,27 @@
+package bagging
+
+import "github.com/Giulianos/ml-decision-tree/classifier"
+
+func (b Bagging) Classify(example classifier.Example) (string, float64) {
+	classifications := make(map[string]float64)
+	for _, decTree := range b.classifiers {
+		class, _ := decTree.Classify(example)
+		classifications[class]++
+	}
+
+	return argMax(classifications), 1.0
+}
+
+func argMax(m map[string]float64) string {
+	var maxVal float64
+	var maxValKey string
+
+	for k, v := range m {
+		if v > maxVal {
+			maxVal = v
+			maxValKey = k
+		}
+	}
+
+	return maxValKey
+}
