a15130a4c66575a3b379d6fb8fb89fc98672ef9f
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-09-17 18:02:16 -0300

    Add method get classes

--- bagging/bagging.go
+++ bagging/bagging.go
@@ -3,12 +3,15 @@ package bagging
 import (
 	"math/rand"
 
+	"github.com/Giulianos/ml-decision-tree/classifier"
+
 	"github.com/Giulianos/ml-decision-tree/decisiontree"
 )
 
 type Bagging struct {
 	predAttr    string
 	classifiers []decisiontree.DecisionTree
+	classes     []string
 	rng         *rand.Rand
 }
 
@@ -19,6 +22,19 @@ func NewBagging(predAttr string, quantity int, seed int64) Bagging {
 	}
 }
 
+func (b *Bagging) saveClasses(examples []classifier.Example) {
+	classesSet := make(map[string]int)
+	for _, example := range examples {
+		class := example[b.predAttr]
+		classesSet[class]++
+	}
+
+	b.classes = make([]string, len(classesSet))
+	for class := range classesSet {
+		b.classes = append(b.classes, class)
+	}
+}
+
 func (b Bagging) GetClasses() []string {
-	return []string{}
+	return b.classes
 }
