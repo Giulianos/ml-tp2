746d112b25dfb0fe5630a29e3f6d78e09985c9ec
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-09-18 19:18:30 -0300

    Fix splitset bugs

--- cmd/splitset/split_set.go
+++ cmd/splitset/split_set.go
@@ -13,19 +13,19 @@ import (
 	"github.com/Giulianos/ml-decision-tree/marshalling"
 )
 
-func randomSplit(examples []classifier.Example, testPortion float64) ([]classifier.Example, []classifier.Example) {
+func randomSplit(examples []classifier.Example, testPortion float64) (train []classifier.Example, test []classifier.Example) {
 	rand.Shuffle(len(examples), func(i, j int) {
 		examples[i], examples[j] = examples[j], examples[i]
 	})
 
 	testSize := int(float64(len(examples)) * testPortion)
 
-	return examples[0:testSize], examples[testSize:]
+	return examples[testSize:], examples[0:testSize]
 }
 
 func writeSet(examples []classifier.Example, filename string) error {
 	// Open file to write
-	file, err := os.Open(filename)
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
@@ -47,14 +47,24 @@ func writeSet(examples []classifier.Example, filename string) error {
 func main() {
 	// Read flags
 	dsFilename := flag.String("ds", "", "input filename for dataset to split, stdin if empty")
-	trainFilename := flag.String("ds", "train.csv", "output filename for train set")
-	testFilename := flag.String("ds", "test.csv", "output filename for test set")
+	trainFilename := flag.String("train", "train.csv", "output filename for train set")
+	testFilename := flag.String("test", "test.csv", "output filename for test set")
+	customSeed := flag.Int64("seed", 1207, "custom seed for random split")
 	flag.Parse()
 
+	// Set seed
+	rand.Seed(*customSeed)
+
 	// Open file to read
-	dsFile, err := os.Open(*dsFilename)
-	if err != nil {
-		log.Fatal(err)
+	var dsFile *os.File
+	if *dsFilename == "" {
+		dsFile = os.Stdin
+	} else {
+		var err error
+		dsFile, err = os.Open(*dsFilename)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	dsReader := bufio.NewReader(dsFile)
 	dsCSVReader := csv.NewReader(dsReader)
