20ddaba3d9bdf76c47f4ed3e88a77cc3ef0cbe31
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-09-20 15:56:30 -0300

    Add precision printer

---
+++ cmd/precision/precision.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Giulianos/ml-decision-tree/decisiontree"
+
+	"github.com/Giulianos/ml-decision-tree/classifier"
+	"github.com/Giulianos/ml-decision-tree/marshalling"
+)
+
+func loadDataset(filename string) ([]classifier.Example, error) {
+	// Open file
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create csv reader
+	csvReader := csv.NewReader(f)
+	examples, err := marshalling.UnmarshallCSV(*csvReader)
+	if err != nil {
+		return nil, err
+	}
+	return examples, nil
+}
+
+func main() {
+	// Log to stderr
+	log.SetOutput(os.Stderr)
+
+	// Read flags
+	trainFilename := flag.String("train", "", "input training set filename (required)")
+	testFilename := flag.String("test", "", "input test set filename (required)")
+	predictableAttr := flag.String("pred-attr", "", "column name of the predictable attribute (required)")
+	flag.Parse()
+
+	// Validate parameters
+	if *trainFilename == "" || *testFilename == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// Load training set
+	training, err := loadDataset(*trainFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Load test set
+	test, err := loadDataset(*testFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create classifier
+	classif := decisiontree.NewDecisionTree(*predictableAttr)
+	classif.SetGainFunction(decisiontree.GINI)
+	classif.SetMinSplitCount(5)
+
+	// Train classifier
+	err = classif.Fit(training)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Run evaluation
+	eval := classifier.EvalClassifier(classif, test)
+
+	// Print nodeCount/precision
+	fmt.Printf("node count: %d , precision: %f", classif.GetNodeCount(), eval.Precision["0"])
+}
