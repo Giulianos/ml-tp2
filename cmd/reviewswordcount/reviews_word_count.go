153e2b19c2ee194534fa2880c73ce9bc90a5dc0a
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-09-20 22:08:48 -0300

    Add program to count 1 star reviews word count avg

---
+++ cmd/reviewswordcount/reviews_word_count.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/Giulianos/ml-decision-tree/classifier"
+	"github.com/Giulianos/ml-decision-tree/marshalling"
+)
+
+func loadDataset(filename string) ([]classifier.Example, error) {
+	// Open file
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create csv reader
+	csvReader := csv.NewReader(f)
+	examples, err := marshalling.UnmarshallCSV(*csvReader)
+	if err != nil {
+		return nil, err
+	}
+	return examples, nil
+}
+
+func averageWordCount(examples []classifier.Example) float64 {
+	var wordCount float64
+	var reviewCount float64
+	for _, example := range examples {
+		words, err := strconv.ParseInt(example["wordcount"], 10, 64)
+		if err == nil {
+			wordCount += float64(words)
+			reviewCount++
+		}
+	}
+	return wordCount / reviewCount
+}
+
+func filter(examples []classifier.Example, keep func(classifier.Example) bool) []classifier.Example {
+	filtered := []classifier.Example{}
+
+	for _, example := range examples {
+		if keep(example) {
+			filtered = append(filtered, example)
+		}
+	}
+
+	return filtered
+}
+
+func main() {
+	// Define flags
+	dsFilename := flag.String("ds", "", "filename of the dataset (required)")
+	flag.Parse()
+
+	// Validate flags
+	if *dsFilename == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// Read dataset
+	ds, err := loadDataset(*dsFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Average word count
+	x := averageWordCount(filter(ds, func(ex classifier.Example) bool {
+		// Filter only one star reviews
+		return ex["Star Rating"] == "1"
+	}))
+
+	fmt.Printf("One star reviews have on average, %f words\n", x)
+
+}
